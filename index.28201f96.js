258d44bf8b3fde195af5a63399eaa8a99c84346f
Iryna <irakogut23190@gmail.com>
2024-12-16 18:46:58 +0200

    make build

---
+++ index.28201f96.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),o=e.offsetHeight/2-25,c=e.offsetWidth/2-25;t.style.top="".concat(o,"px"),t.style.left="".concat(c,"px");
+//# sourceMappingURL=index.28201f96.js.map
