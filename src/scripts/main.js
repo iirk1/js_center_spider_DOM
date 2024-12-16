36530d22b9547c9dbc1f0018fbaf26e4637f4e21
Iryna <irakogut23190@gmail.com>
2024-12-16 18:46:04 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,8 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const positionLeft = wall.offsetWidth / 2 - 25;
 const positionRight = wall.offsetHeight / 2 - 25;
+const positionLeft = wall.offsetWidth / 2 - 25;
 
-spider.style.left = `${positionLeft}px`;
 spider.style.top = `${positionRight}px`;
+spider.style.left = `${positionLeft}px`;
